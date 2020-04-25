95da0a121470902bc1e29ff656b48452f943a997
gauravm-bit <gauravwrk01@gmail.com>
2020-04-25 11:44:42 +0530

    ADDED:test case to unpark the car

--- app/ParkingLot.js
+++ app/ParkingLot.js
@@ -1,6 +1,24 @@
 class ParkingLot {
     
-    doParking = (car) => { return true };
+    constructor(){
+        this.Lot = new Array(100);
+    }
+
+    doParking = (car) => { 
+        this.Lot.push(car)
+
+        return true;
+    }
+
+    doUnpark = (car) => {
+        if(this.Lot.includes(car)){
+            this.Lot.pop(car)
+            return true;
+        }
+        
+    }
+    
 
 }
+ 
 module.exports = new ParkingLot;
--- test/TestParkingLot.js
+++ test/TestParkingLot.js
@@ -1,14 +1,19 @@
 const assert = require('chai').assert
 const parkingLot = require('../app/ParkingLot.js')
 
-let car = { }
-describe(`Testing for Parking Lot service`, () => {
+let car = { } 
+
+describe( `Testing for Parking Lot service`, () => {
 
 //TC 1.1 let the driver park so that he can board his flight
 it(`allow parking so the driver can board flight`, () => {
     let result = parkingLot.doParking(car)
     assert.isTrue(result)
 })
-
+//TC2.1 let the driver unpark so he can go home
+it(`allow unparking so the driver can home`, () => {
+    let result = parkingLot.doUnpark(car)
+    assert.isTrue(result)
+})
 
 })
